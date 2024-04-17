e7cfa547a170fae6bd67efda5c05d2f6df3203e7
Hitesh7987322 <146938420+Hitesh7987322@users.noreply.github.com>
2024-04-17 14:20:21 +0530

    Add files via upload

---
+++ AI-ChatBotProj/script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// const countvowel = (str) => {
+//     let vowel = ['a','e','i','o','u'];
+//     let arr = str.split('');
+//     console.log(arr);
+//     let count = 0;
+//     for(let char of arr){
+//         if(vowel.includes(char.toLowerCase())){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log("hello hitesh");
+
+
